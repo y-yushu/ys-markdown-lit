125d5587f8e4c72d3236d50d3caf2bd109f60893
y-yushu <2671255784@qq.com>
2025-02-13 00:05:46 +0800

    feat: 项目初步创建完成

--- src/my-element.js
+++ src/my-element.js
@@ -1,24 +1,11 @@
-import { LitElement, css, html } from 'lit'
+import { LitElement, html, unsafeCSS } from 'lit'
+import tailwindStyles from './index.css?inline'
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 export class MyElement extends LitElement {
-  static get properties() {
-    return {
-      /**
-       * Copy for the read the docs hint.
-       */
-      docsHint: { type: String },
+  static styles = [unsafeCSS(tailwindStyles)]
 
-      /**
-       * The number of times the button has been clicked.
-       */
-      count: { type: Number }
-    }
+  static get properties() {
+    return {}
   }
 
   constructor() {
@@ -27,27 +14,22 @@ export class MyElement extends LitElement {
     this.count = 0
   }
 
-  render() {
-    return html`<h1 class="text-3xl font-bold underline">Hello world!</h1>
+  getMod() {
+    return html`
       <div
         class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
       >
-        <img class="size-12 shrink-0" src="/img/logo.svg" alt="ChitChat Logo" />
         <div>
           <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
           <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
         </div>
-      </div>`
-  }
-
-  _onClick() {
-    this.count++
-  }
-
-  static get styles() {
-    return css`
-      @import 'tailwindcss';
+      </div>
     `
+  }
+
+  render() {
+    return html`<h1 class="text-3xl font-bold text-red-400 underline">Hello world!</h1>
+      ${this.getMod()} `
   }
 }
 
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import lit from 'vite-plugin-lit'
-
-export default defineConfig({
-  plugins: [lit(), tailwindcss()]
-})
