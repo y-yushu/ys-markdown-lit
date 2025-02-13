2ea12d199c7fab4eda7471c917fd58c2be46108c
y-yushu <2671255784@qq.com>
2025-02-13 23:36:27 +0800

    feat: 添加markdown-it

--- src/components/my-element/index.ts
+++ src/components/my-element/index.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, unsafeCSS } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import MarkdownIt from 'markdown-it'
 import tailwindStyles from './index.css?inline'
 
 @customElement('my-element')
@@ -7,23 +8,16 @@ export class MyElement extends LitElement {
   static styles = [unsafeCSS(tailwindStyles)]
 
   @property()
-  content = 'Hello world!'
+  content = '# Hello world!'
 
   constructor() {
     super()
   }
 
   getMod() {
-    return html`
-      <div
-        class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
-      >
-        <div>
-          <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
-          <p class="text-gray-500 dark:text-gray-400">${this.content}!</p>
-        </div>
-      </div>
-    `
+    const md = new MarkdownIt()
+    const _html = md.render(this.content)
+    return html`${_html}`
   }
 
   render() {
