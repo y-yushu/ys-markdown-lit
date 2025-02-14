9b1aabea2ca2f4dc338fb8366653418826c2721a
y-yushu <2671255784@qq.com>
2025-02-15 00:38:54 +0800

    feat: implement Markdown rendering with AST in MyElement component

--- src/components/my-element/index.ts
+++ src/components/my-element/index.ts
@@ -2,26 +2,53 @@ import { LitElement, html, unsafeCSS } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import MarkdownIt from 'markdown-it'
 import tailwindStyles from './index.css?inline'
+import { MarkdownStr } from './mock'
 
 @customElement('my-element')
 export class MyElement extends LitElement {
   static styles = [unsafeCSS(tailwindStyles)]
 
-  @property()
-  content = '# Hello world!'
+  @property({ type: String })
+  content = MarkdownStr
 
   constructor() {
     super()
   }
 
-  getMod() {
+  getAST(): unknown[] {
     const md = new MarkdownIt()
-    const _html = md.render(this.content)
-    return html`${_html}`
+    const ast: MarkdownIt.Token[] = md.parse(this.content, {})
+    console.log('抽象语法树\n', ast)
+    return this.renderAst(ast)
+  }
+
+  // 渲染AST
+  renderAst(ast: MarkdownIt.Token[] | null): unknown[] {
+    if (!ast) return []
+
+    return ast.map((node: MarkdownIt.Token) => {
+      if (node.type === 'text') {
+        return node.content
+      } else if (node.type === 'inline') {
+        return this.renderAst(node.children)
+      } else if (node.type === 'heading_open') {
+        return html`<h${node.tag}>${this.renderAst(node.children)}</h${node.tag}>`
+      } else if (node.type === 'paragraph_open') {
+        return html`<p>${this.renderAst(node.children)}</p>`
+      } else if (node.type === 'link_open') {
+        const hrefAttr = node.attrs?.find((attr: [string, string]) => attr[0] === 'href')
+        if (!hrefAttr) return null
+        return html`<a href=${hrefAttr[1]}>${this.renderAst(node.children)}</a>`
+      } else {
+        console.log('[type类型未判断]', node.type, node)
+      }
+
+      return null
+    })
   }
 
   render() {
-    return html`<div class="">${this.getMod()}</div> `
+    return html`<div>${this.getAST()}</div> `
   }
 }
 
---
+++ src/components/my-element/mock.ts
@@ -0,0 +1,7 @@
+export const MarkdownStr: string = `# Hello world!
+
+|名称|属性|
+|--|--|
+|code|\`hello worde\`|
+|加粗|*12
+`
